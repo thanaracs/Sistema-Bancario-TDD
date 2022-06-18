1d13091b7a89098306d1d9dcffd61e8571b76890
Thainara Carvalho <thai.cds1@gmail.com>
2022-06-18 13:17:29 -0400

    Implementação feita segundo video. Feito o método depositoDinheiro

--- src/main/java/com/sistemabancario/model/Conta.java
+++ src/main/java/com/sistemabancario/model/Conta.java
@@ -92,7 +92,12 @@ public class Conta implements Cadastro {
      * @param movimentacao {@link Movimentacao} a ser adicionada
      */
     public void addMovimentacao(Movimentacao movimentacao) {
-        // TODO: Você precisa implementar este método
+        if(movimentacao.isConfirmada()){
+            if(movimentacao.getTipo() == 'C')
+                saldo += movimentacao.getValor();
+            else if(movimentacao.getTipo() == 'D')
+                saldo -= movimentacao.getValor();
+        }
     }
 
     /**
@@ -102,9 +107,7 @@ public class Conta implements Cadastro {
      * @return
      */
     public double getSaldoTotal() {
-        /* TODO: Você precisa implementar este método. 
-        A linha abaixo deve ser substituída pelo seu código */
-        return 0.0;
+        return saldo + limite;
     }
 
     /**
@@ -124,8 +127,16 @@ public class Conta implements Cadastro {
      *
      * @param valor valor a ser sacado (deve ser um valor positivo)
      */
+
+
+
     public void saque(final double valor) {
-        // TODO: Você precisa implementar este método
+            Movimentacao movimentacao = new Movimentacao(this);
+            movimentacao.setConfirmada(true);
+            movimentacao.setTipo('D');
+            movimentacao.setValor(valor);
+            saldo -= valor;
+            movimentacoes.add(movimentacao);
     }
 
     /**
@@ -136,8 +147,16 @@ public class Conta implements Cadastro {
      *
      * @param valor valor a ser depositado (deve ser um valor positivo)
      */
-    public void depositoDinheiro(final double valor) {
-        // TODO: Você precisa implementar este método
+    public void depositoDinheiro(double valor) {
+            if (valor < 0){
+                throw new IllegalArgumentException("Valor de deposito nao pode ser negativo");
+            }
+            Movimentacao movimentacao = new Movimentacao(this);
+            movimentacao.setConfirmada(true);
+            movimentacao.setTipo('C');
+            movimentacao.setValor(valor);
+            saldo += valor;
+            movimentacoes.add(movimentacao);
     }
 
     /**
@@ -202,3 +221,5 @@ public class Conta implements Cadastro {
         this.limite = limite;
     }
 }
+
+
--- src/test/java/com/sistemabancario/model/ContaTest.java
+++ src/test/java/com/sistemabancario/model/ContaTest.java
@@ -63,5 +63,86 @@ public class ContaTest {
         final Conta instance = new Conta();
         assertNotNull(instance.getMovimentacoes());
     }
+    @Test
+    void testGetSaldoTotal(){
+        final double limite = 500;
+        final double esperado = limite;
+        final Conta instance = new Conta();
+        instance.setEspecial(true);
+        instance.setLimite(limite);
+        final double obtido = instance.getSaldoTotal();
+        assertEquals(esperado, obtido);
+    }
 
+    @Test
+    void testDepositoDinheiro(){
+        final double limite = 500.6, deposito = 500.8, esperado = 1001.4;
+        final Conta instance = new Conta();
+        instance.setEspecial(true);
+        instance.setLimite(limite);
+        instance.depositoDinheiro(deposito);
+
+        final double obtido = instance.getSaldoTotal();
+        assertEquals(esperado, obtido, 0.001);
+    }
+
+    /* Movimentação adicionada na lista */
+    @Test
+    void testAddMovimentacaoCredito() {
+        Conta instance = new Conta();
+        Movimentacao mv = new Movimentacao(instance);
+        mv.setConfirmada(true);
+        mv.setTipo('C');
+        final double esperado = 100.50;
+        mv.setValor(esperado);
+        instance.addMovimentacao(mv);
+        assertEquals(esperado, instance.getSaldoTotal());
+    }
+
+    /* Verifica se tipo movimentação foi definida como credito */
+    @Test
+    void testTipoMovimentacaoCredito() {
+        Conta instance = new Conta();
+        Movimentacao mv = new Movimentacao(instance);
+        mv.setConfirmada(true);
+        mv.setTipo('C');
+        final char esperado = 'C';
+        instance.addMovimentacao(mv);
+        assertEquals(esperado, mv.getTipo());
+    }
+
+    /* Teste de valor atribuído */
+    @Test
+    void testValorAtribuido() {
+        Conta instance = new Conta();
+        Movimentacao mv = new Movimentacao(instance);
+        final double esperado = 200;
+        mv.setConfirmada(true);
+        mv.setTipo('C');
+        mv.setValor(esperado);
+        instance.addMovimentacao(mv);
+        assertEquals(esperado, mv.getValor());
+    }
+
+    /* Teste Deposito confirmado */
+    @Test
+    void testDepositoConfirmado() {
+        Conta instance = new Conta();
+        Movimentacao mv = new Movimentacao(instance);
+        final boolean esperado = true;
+        mv.setConfirmada(true);
+        mv.setTipo('C');
+        instance.addMovimentacao(mv);
+        assertEquals(esperado, mv.isConfirmada());
+    }
+
+    //Deposito de Valor Negativo
+    @Test
+    void testDepositoDinheiroValorNegativo(){
+        final double limite = 500.6, deposito = -500;
+        final Conta instance = new Conta();
+        instance.setEspecial(true);
+        instance.setLimite(limite);
+        assertThrows(IllegalArgumentException.class, () -> instance.depositoDinheiro(deposito));
+    }
 }
